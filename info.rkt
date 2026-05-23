fd040829c6e2d782faaaa36998e7121cfb22aed7
Matthias Felleisen <matthias@ccs.neu.edu>
2026-05-22 20:15:57 -0500

    create package

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "Xmanage")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+; (define scribblings '(("Scribblingsh/xmanage.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(matthias))
+(define license '(Apache-2.0 OR MIT))
---
+++ scribblings/Xmanage.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[Xmanage
+                    racket/base]]
+
+@title{Xmanage}
+@author{Matthias Felleisen}
+
+@defmodule[Xmanage]
+
+Package Description Here
