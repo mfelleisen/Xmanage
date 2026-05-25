47d93a002fa015b5c9ed9990a97017f4c08356e5
Matthias Felleisen <matthias@ccs.neu.edu>
2026-05-25 15:53:59 -0500

    fix contract

--- xmanage.rkt
+++ xmanage.rkt
@@ -227,7 +227,7 @@ exec racket -tm "$0" -- ${1+"$@"}
      (check-main "make TTT" 0 "" "-new" TTT))
    (λ ()
      ;; re-create to trigger failure
-     (check-main "bad check/withdrawal amount" 1 "amount expected" TTT "-c" "0.00"))
+     (check-main "no purpose statement" 1 "amount expected" TTT "-c" "0.00"))
    (λ ()
      (delete-file (~a "." TTT ".act"))))
   
